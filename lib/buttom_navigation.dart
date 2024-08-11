04e11e3d00b68ec21268d0c80559f60468967d60
Rifat Hasan Priyo <137903375+rifathasanpriyo@users.noreply.github.com>
2024-08-11 19:00:06 +0600

    bottom_navigation_bar

---
+++ lib/buttom_navigation.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class ButtonNavigation extends StatefulWidget {
+  const ButtonNavigation({super.key});
+
+  @override
+  State<ButtonNavigation> createState() => _ButtonNavigationState();
+}
+
+class _ButtonNavigationState extends State<ButtonNavigation> {
+  int index = 0;
+  List<Widget> listMenu = [
+    Icon(Icons.home, size: 150),
+    Icon(Icons.favorite, size: 150),
+    Icon(Icons.person, size: 150),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: listMenu[index]),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: index,
+        onTap: (int newIndex) {
+          setState(() {
+            index = newIndex;
+          });
+        },
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite),
+            label: 'Favorite',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/button_navigation.dart';
 
 void main() {
   runApp(const MyApp());
@@ -32,7 +33,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Donate to Palestine',),
+      home: ButtonNavigation(),
     );
   }
 }
